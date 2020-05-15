1b51c35a604b1c8042b7a917cfc4fdc4aea048a1
Sergiy Popovych <sergiy.popovich@gmail.com>
2020-05-15 14:16:17 -0700

    yo

--- modelhouse/loading/loading.py
+++ modelhouse/loading/loading.py
@@ -1,6 +1,7 @@
 import tempfile
 import shutil
 import os
+import sys
 import importlib
 
 import modelhouse
@@ -10,6 +11,7 @@ def import_file(module_name, file_path):
     prev_cwd = os.getcwd()
     os.chdir(os.path.dirname(file_path))
 
+    sys.path.insert(1, os.path.dirname(file_path))
     spec = importlib.util.spec_from_file_location(module_name, file_path)
     module = importlib.util.module_from_spec(spec)
 
@@ -18,14 +20,16 @@ def import_file(module_name, file_path):
     os.chdir(prev_cwd)
     return module
 
-def uncached_load_model(model_path, model_params={}):
+def uncached_load_model(path, **params):
     # create temp dir
     tmp_dir_path = tempfile.mkdtemp(dir=modelhouse.MODELHOUSE_TMP_FILES_DIR)
 
-    files_and_contents = get_files_and_contents(model_path)
+    files_and_contents = get_files_and_contents(path)
+    if len(files_and_contents) == 0:
+        raise Exception(f"No model found at {path}")
     put_files_and_contents(tmp_dir_path, files_and_contents)
     creator_path = os.path.join(tmp_dir_path, "create.py")
     creator = import_file("create", creator_path)
-    model = creator.create(**model_params)
-    shutil.rmtree(tmp_dir_path)
+    model = creator.create(**params)
+    #shutil.rmtree(tmp_dir_path)
     return model
